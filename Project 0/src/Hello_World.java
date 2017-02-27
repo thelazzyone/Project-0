c011425fc379abf8fbd62311ce04a1d214f87f35
thelazzyone <eddielam27@gmail.com>
2017-02-26 22:12:19 -0500

    Adding a new Project

---
+++ Project 0/src/Hello_World.java
@@ -0,0 +1,8 @@
+
+public class Hello_World {
+	public static void main( String[] args ) {
+		System.out.println ("Hello, World!");
+		
+		System.out.println ("\nPart 4 of Step 3, adding more lines to Java.");
+	  }
+}
